ba0e2db97f90058c9a24e26a424a443ef8d5a00e
soricine <soricine@gmail.com>
2021-09-21 20:31:57 +0300

    first push chris

--- lnd/lnd.go
+++ lnd/lnd.go
@@ -55,6 +55,13 @@ import (
 	"github.com/pkt-cash/pktd/lnd/watchtower/wtdb"
 )
 
+const (
+	// blockDbNamePrefix is the prefix for the block database name.  The
+	// database type is appended to this value to form the full block
+	// database name.
+	blockDbNamePrefix = "blocks"
+)
+
 // WalletUnlockerAuthOptions returns a list of DialOptions that can be used to
 // authenticate with the wallet unlocker service.
 //
